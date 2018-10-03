22717a2e4a67d7275a06464f147d1c48e220e7e3
sajjad782 <sajjad_ahmed782@yahoo.com>
2018-10-04 00:54:26 +0500

    session 14: Arthematic Operations
    Signed-off-by: sajjad782 <sajjad_ahmed782@yahoo.com>

---
+++ session_14/arthematic00.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a, b, c;
+    
+    b = 2; c = 3;
+    a = b + c;
+    cout << "a = b + c " << a << endl;
+
+    a = b / c;
+    cout << "a = b / c " << a << endl;
+}
