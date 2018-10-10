841786b9ec54ccefd503f02e55d36e1818f7a04a
sajjad782 <sajjad_ahmed782@yahoo.com>
2018-10-11 04:21:54 +0500

    for-loop: Multi expressions in a for loop
    Signed-off-by: sajjad782 <sajjad_ahmed782@yahoo.com>

---
+++ session_20/multi-express-loop.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/* This program is nothing but about properties of for loop
+how we can use the initialization, condition and increment areas */
+
+int main()
+{
+	for (int a = 0; a < 3; a ++, cout << "a = " << a << endl)
+	{}
+}
